aa54fe7853476b9926843eb2324bd6816abed66f
leo-geindre <leo.geindre@hotmail.fr>
2020-11-01 15:39:07 +0100

    Commit td02

---
+++ td02/src/main/java/fr/dauphine/ja/geindreleo/td02/Fille.java
@@ -0,0 +1,7 @@
+package fr.dauphine.ja.geindreleo.td02;
+
+class Fille extends Mere {
+	public int meth() {
+		return 24;
+	}
+}
---
+++ td02/src/main/java/fr/dauphine/ja/geindreleo/td02/Mere.java
@@ -0,0 +1,10 @@
+package fr.dauphine.ja.geindreleo.td02;
+
+class Mere {
+	protected int meth() {
+		return 42;
+	}
+	public void printMeth() {
+		System.out.println(meth());
+	}
+}
